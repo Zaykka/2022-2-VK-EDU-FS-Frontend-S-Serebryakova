b6f058f5e73973b3c676511233b68f6ea2e91ce2
Zaykka <zay082008@gmail.com>
2022-10-13 15:18:15 +0300

    add hw2

---
+++ hw2/simple-chat/src/index.js
@@ -0,0 +1,65 @@
+const form = document.querySelector('form');
+const input = document.querySelector('.form-input');
+const chat = document.querySelector('.messages');
+
+form.addEventListener('submit', this.handleSubmit.bind(this));
+document.addEventListener('DOMContentLoaded', this.getMesagesFromLocalStorage.bind(this));
+
+function getMesagesFromLocalStorage() {
+  let messages = localStorage.getItem('messages');
+  if (messages == '' || messages == null) {
+    return
+  } 
+  messages = JSON.parse(messages);
+  for (let message of messages.all) {
+    createMessageBlock(message, save=false);
+  }
+}
+
+function saveMessageToLocalStorage(message) {
+  let messages = localStorage.getItem('messages');
+  if (messages == '' || messages == null) {
+    localStorage.setItem('messages', JSON.stringify({'all':[]}));
+  }
+  messages = localStorage.getItem('messages');
+  messages = JSON.parse(messages);
+  messages.all.push(message);
+  localStorage.setItem('messages', JSON.stringify(messages));
+}
+
+function createMessageBlock(message, save=true) {
+    let message_block = document.createElement('div');
+    message_block.className = 'message';
+     
+    let message_text = document.createElement('div');
+    message_text.className = "message-text";
+    message_text.innerText = message.text;
+    message_block.append(message_text);
+
+    let message_meta = document.createElement('div');
+    let message_date = document.createElement('div');
+    message_meta.className = "message-meta";
+
+    message_date.className = "message-date";
+    message_date.innerText = message.date;
+
+    message_meta.append(message_date);
+
+    message_block.append(message_meta);
+    
+    chat.append(message_block)
+    if (save) {
+      saveMessageToLocalStorage(message);
+    }
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let time = new Date();
+    let message = {
+      'text': input.value,
+      'date': `${time.getHours()}:${time.getMinutes()}`
+    };
+    createMessageBlock(message);
+}
+
