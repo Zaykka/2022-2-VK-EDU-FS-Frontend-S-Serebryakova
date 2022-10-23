3643d3798f0fc3089aebcc4204d399c648868627
Zaykka <Zaykka@users.noreply.github.com>
2022-10-23 14:57:11 +0000

    deploy: 57b0afbe121cfd1deda570b780af2b826b7497d7

--- bundle.js
+++ bundle.js
@@ -81,31 +81,64 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./index.js");
+/******/ 	return __webpack_require__(__webpack_require__.s = "./chat_list/js/index.js");
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ "./index.css":
-/*!*******************!*\
-  !*** ./index.css ***!
-  \*******************/
+/***/ "./chat_list/css/chat.css":
+/*!********************************!*\
+  !*** ./chat_list/css/chat.css ***!
+  \********************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./index.css?");
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./chat_list/css/chat.css?");
 
 /***/ }),
 
-/***/ "./index.js":
-/*!******************!*\
-  !*** ./index.js ***!
-  \******************/
+/***/ "./chat_list/css/float-button.css":
+/*!****************************************!*\
+  !*** ./chat_list/css/float-button.css ***!
+  \****************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./chat_list/css/float-button.css?");
+
+/***/ }),
+
+/***/ "./chat_list/css/header.css":
+/*!**********************************!*\
+  !*** ./chat_list/css/header.css ***!
+  \**********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./chat_list/css/header.css?");
+
+/***/ }),
+
+/***/ "./chat_list/css/page.css":
+/*!********************************!*\
+  !*** ./chat_list/css/page.css ***!
+  \********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./chat_list/css/page.css?");
+
+/***/ }),
+
+/***/ "./chat_list/js/index.js":
+/*!*******************************!*\
+  !*** ./chat_list/js/index.js ***!
+  \*******************************/
 /*! no exports provided */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index.css */ \"./index.css\");\n/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_index_css__WEBPACK_IMPORTED_MODULE_0__);\nfunction _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== \"undefined\" && o[Symbol.iterator] || o[\"@@iterator\"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === \"number\") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError(\"Invalid attempt to iterate non-iterable instance.\\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.\"); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it[\"return\"] != null) it[\"return\"](); } finally { if (didErr) throw err; } } }; }\nfunction _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === \"string\") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === \"Object\" && o.constructor) n = o.constructor.name; if (n === \"Map\" || n === \"Set\") return Array.from(o); if (n === \"Arguments\" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }\nfunction _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }\n\nvar form = document.querySelector('form');\nvar input = document.querySelector('.form-input');\nvar chat = document.querySelector('.messages');\nform.addEventListener('submit', handleSubmit.bind(undefined));\ndocument.addEventListener('DOMContentLoaded', getMesagesFromLocalStorage.bind(undefined));\nfunction getMesagesFromLocalStorage() {\n  var messages = localStorage.getItem('messages');\n  if (messages == '' || messages == null) {\n    return;\n  }\n  messages = JSON.parse(messages);\n  var _iterator = _createForOfIteratorHelper(messages.all),\n    _step;\n  try {\n    for (_iterator.s(); !(_step = _iterator.n()).done;) {\n      var message = _step.value;\n      createMessageBlock(message, false);\n    }\n  } catch (err) {\n    _iterator.e(err);\n  } finally {\n    _iterator.f();\n  }\n}\nfunction saveMessageToLocalStorage(message) {\n  var messages = localStorage.getItem('messages');\n  if (messages == '' || messages == null) {\n    localStorage.setItem('messages', JSON.stringify({\n      'all': []\n    }));\n  }\n  messages = localStorage.getItem('messages');\n  messages = JSON.parse(messages);\n  messages.all.push(message);\n  localStorage.setItem('messages', JSON.stringify(messages));\n}\nfunction createMessageBlock(message, save) {\n  var message_block = document.createElement('div');\n  message_block.className = 'message';\n  var message_text = document.createElement('div');\n  message_text.className = \"message-text\";\n  message_text.innerText = message.text;\n  message_block.append(message_text);\n  var message_meta = document.createElement('div');\n  var message_date = document.createElement('div');\n  message_meta.className = \"message-meta\";\n  message_date.className = \"message-date\";\n  message_date.innerText = message.date;\n  message_meta.append(message_date);\n  message_block.append(message_meta);\n  chat.append(message_block);\n  if (save) {\n    saveMessageToLocalStorage(message);\n  }\n}\nfunction handleSubmit(event) {\n  event.preventDefault();\n  var time = new Date();\n  var message = {\n    'text': input.value,\n    'date': \"\".concat(time.getHours(), \":\").concat(time.getMinutes())\n  };\n  createMessageBlock(message, true);\n  input.value = '';\n}\n\n//# sourceURL=webpack:///./index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _css_chat_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../css/chat.css */ \"./chat_list/css/chat.css\");\n/* harmony import */ var _css_chat_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_css_chat_css__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _css_float_button_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../css/float-button.css */ \"./chat_list/css/float-button.css\");\n/* harmony import */ var _css_float_button_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_css_float_button_css__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _css_header_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../css/header.css */ \"./chat_list/css/header.css\");\n/* harmony import */ var _css_header_css__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_css_header_css__WEBPACK_IMPORTED_MODULE_2__);\n/* harmony import */ var _css_page_css__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../css/page.css */ \"./chat_list/css/page.css\");\n/* harmony import */ var _css_page_css__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_css_page_css__WEBPACK_IMPORTED_MODULE_3__);\n\n\n\n\n\n//# sourceURL=webpack:///./chat_list/js/index.js?");
 
 /***/ })
 
